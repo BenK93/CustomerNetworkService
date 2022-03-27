27dd044e405a6c1c22abfa95645afec81b4f5621
ben.koren <ben.koren@urban-dashboard.com>
2022-03-27 09:40:57 +0300

    work in progress on customer model

--- core/settings.py
+++ core/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'rest_framework',
+    'customers',
+    'multiselectfield',
 ]
 
 MIDDLEWARE = [
--- core/urls.py
+++ core/urls.py
@@ -18,5 +18,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/', include('customers.urls'))
+
 ]
