28c6e24d0cef8b4be0732b40a7d88826bf75132c
Sagi Ziv <sagi.ziv@s.afeka.ac.il>
2022-03-31 01:49:33 +0300

    Fixed get by email issue and added some stuff

--- README.md
+++ README.md
@@ -6,17 +6,18 @@
 - pip 
 
 # Installation:  
-   ### Using Docker:
+   ## Using Docker:
    1. `docker build --tag <IMAGE_NAME> .`
-   2. `docker run --publish 8000:8000 <IMAGE_NAME>`
-   3. `docker-compose build`
-   4. `docker-compose up`
-   #### On New Terminal Window
-   5. `docker exec -it <CONTAINER_ID> python manage.py makemigrations`
-   6. `docker exec -it <CONTAINER_ID> python manage.py migrate`
+   1. `docker run --publish 8000:8000 <IMAGE_NAME>`
+   1. `docker-compose build`
+   1. `docker-compose up`
+   ### On New Terminal Window
+   1. `docker exec -it <CONTAINER_ID> python manage.py makemigrations`
+   1. `docker exec -it <CONTAINER_ID> python manage.py migrate`
+   1. docker exec -it 7680d80ea1a7 python manage.py migrate --run-syncdb
 
 
-   ### Running on VM:
+   ## Running on VM:
    1. python3 -m venv venv
    2. source venv/bin/activate
    3. pip3 install requirements.txt
--- customers/api/views/customerView.py
+++ customers/api/views/customerView.py
@@ -1,17 +1,18 @@
-from django.shortcuts import get_object_or_404
 from rest_framework import permissions, status, viewsets
 from rest_framework.generics import RetrieveAPIView
 from rest_framework.response import Response
 
-from customers.api.serializers.customersSerializers import  CustomerRetrieveSerializer, \
-    CustomerCreateSerializer
+from customers.api.serializers.customersSerializers import CustomerCreateSerializer, CustomerRetrieveSerializer
 from customers.models import Customer
+
 
 # to creator boundary
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset = Customer.objects.all()
     permission_classes = [permissions.AllowAny, ]
     lookup_field = 'email'
+    # Source: https://stackoverflow.com/q/55714961
+    lookup_value_regex = r'[^/]+'
 
     def get_serializer_class(self):
         if self.action == 'create':
@@ -32,20 +33,12 @@ class CustomerViewSet(viewsets.ModelViewSet):
             del response['password']
             return Response(response, status=status.HTTP_201_CREATED)
 
-    # def retrieve(self, request, *args, **kwargs):
-    #     print('^^^^^^^^^^^^^^^')
-    #     print(kwargs['pk'])
-    #     return Response({}, status=status.HTTP_404_NOT_FOUND)
-
 
 # getter boundary
 class CustomerRetrieveByEmail(RetrieveAPIView):
     queryset = Customer.objects.all()
     serializer_class = CustomerRetrieveSerializer
     lookup_field = 'email'
-
-
-
 
 
 class CustomerLogin(RetrieveAPIView):
@@ -64,4 +57,3 @@ class CustomerLogin(RetrieveAPIView):
             return Response(status=status.HTTP_404_NOT_FOUND)
         serializer = self.get_serializer(instance)
         return Response(serializer.data)
-
